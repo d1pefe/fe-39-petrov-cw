4e16aec185c2188452f1c48c495a24a0ac96631f
d1pefe <113125565+d1pefe@users.noreply.github.com>
2023-02-13 14:11:05 +0300

    training made style in .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+const Title = (props: any) => {
+    return <div>{props.text}</div>;
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{
+          flexDirection: "column",
+          flex: 1,
+          backgroundColor: "red",
+      }}>
         <img src={logo} className="App-logo" alt="logo" />
+        <Title text={`Edit src/App.tsx and save to reload.`} />
       </header>
     </div>
   );
